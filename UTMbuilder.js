af844a387901fbf6a99ea8b2f9c06fa5518c7e8c
Dan Bonomo <d.z.bonomo@gmail.com>
2017-04-19 22:56:24 -0700

    Forces Lowercase, Limits URL to under 2k char

--- UTMbuilder.js
+++ UTMbuilder.js
@@ -25,35 +25,39 @@ function track(url,source,medium,term,content,campaign) {
   } else {
     var source_query = "?utm_source=";
   }
-  var e_source = encodeURIComponent(source);
+  var e_source = encodeURIComponent(source.toLowerCase());
   if (medium !== "") {
     var medium_query = "&utm_medium=";
-    var e_medium = encodeURIComponent(medium);
+    var e_medium = encodeURIComponent(medium.toLowerCase());
   } else {
     var medium_query = "";
     var e_medium = "";
   }
   if (term !== "") {
     var term_query = "&utm_term=";
-    var e_term = encodeURIComponent(term);
+    var e_term = encodeURIComponent(term.toLowerCase());
   } else {
     var term_query = "";
     var e_term = "";
   }
   if (content !== "") {
     var content_query = "&utm_content=";
-    var e_content = encodeURIComponent(content);
+    var e_content = encodeURIComponent(content.toLowerCase());
   } else {
     var content_query = "";
     var e_content = "";
   }
   if (campaign !== "") {
     var campaign_query = "&utm_campaign=";
-    var e_campaign = encodeURIComponent(campaign);
+    var e_campaign = encodeURIComponent(campaign.toLowerCase());
   } else {
     var campaign_query = "";
     var e_campaign = "";
   }
   var combined = url.concat(source_query,e_source,medium_query,e_medium,term_query,e_term,content_query,e_content,campaign_query,e_campaign);
-  return combined;
+  if (combined.length > 2000) {
+    throw new Error("URL must not exceed 2000 characters");
+  } else {
+    return combined;
+  }
 }
